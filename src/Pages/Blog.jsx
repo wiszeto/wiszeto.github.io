be58583f8878e59d08df0c9709e6ddcdb9db049f
Wilson <wilsonszerto99@gmail.com>
2023-08-11 02:32:06 -0700

    pages addition

---
+++ src/Pages/Blog.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import './css/Blog.css'; // Import the CSS file if you have one
+
+const Blog = () => {
+  return (
+    <div className="blog">
+      <h1>Welcome to My Blog</h1>
+      <p>This is where I'll share my thoughts, updates, and more.</p>
+      {/* Add more content and components here */}
+    </div>
+  );
+};
+
+export default Blog;
