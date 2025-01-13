7d37f475e47a7d7cbfec7a1ada911810dd1afc51
Wilson Szeto <wilsonszerto99@gmail.com>
2025-01-13 04:46:40 -0800

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 // Navbar.jsx
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../css/navbar.css' // Import your custom stylesheet
+import '../css/Navbar.css' // Import your custom stylesheet
 
 function Navbar() {
   return (
